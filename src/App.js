cdbdaefd52135a4d7af3a1289d03900ec598dd4e
User <surussab2.0@gmail.com>
2024-03-05 20:01:19 -0300

    Feat: Add app structure

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen w-screen flex justify-center items-center">
+      <div className='card w-[80%] h-[70%] shadow-md rounded-sm transform ease-out duration-500 items-center p-10 gap-5'>
+        <h1 className='text-5x1 font-work font-bold w-fit text-center'>Quest to do</h1>
+        <p>Entrada</p>
+        <p>Lista de quests</p>
+          <input type="reset" value="Clear" class="btn btn-success"/>
+      </div>
     </div>
   );
 }
