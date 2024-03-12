de3622eda80bb4a4bce82d83d9b2bb13e6ff0c0e
User <surussab2.0@gmail.com>
2024-03-12 19:33:04 -0300

    Feat: Add quest input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import logo from './AddQuest';
+
 import './App.css';
+import AddQuest from './AddQuest';
 
 function App() {
+  const[questList, setQuestList] = useState([]);
+
+function addNewQuest(questTitle){
+  const quest = questList.slice();
+
+  quest.push({
+    title: questTitle,
+    done:false
+  });
+
+  setQuestList(quests);
+  console.log(quests);
+
+}
+
   return (
     <div className="App h-screen w-screen flex justify-center items-center">
       <div className='card w-[80%] h-[70%] shadow-md rounded-sm transform ease-out duration-500 items-center p-10 gap-5'>
         <h1 className='text-5x1 font-work font-bold w-fit text-center'>Quest to do</h1>
-        <p>Entrada</p>
+        <AddQuest saveNewQuest={addNewQuest}></AddQuest>
         <p>Lista de quests</p>
           <input type="reset" value="Clear" class="btn btn-success"/>
       </div>
